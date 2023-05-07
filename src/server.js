1511437195fdac71f80c82ff9e3eab052f7fd098
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-07 16:08:08 -0300

    create routes tasks with method http get and post

--- src/server.js
+++ src/server.js
@@ -1,1 +1,32 @@
-console.log('oie!')
+import http from 'node:http'
+import { json } from '../middlewares/json.js'
+
+const users = []
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  await json(req, res)
+
+  if (method === 'GET' && url === '/tasks') {
+    return res
+    .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/tasks') {
+    const { title, description } = req.body
+    users.push({
+      id: 1,
+      title,
+      description,
+      completed_at: null,
+      created_at: new Date(),
+      updated_at: new Date(),
+    })
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
+})
+
+server.listen(3333)
