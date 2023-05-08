b5fefa366d04f00391ab2ef78175d272650f9014
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-08 10:43:42 -0300

    persisting data tasks in database

--- src/server.js
+++ src/server.js
@@ -1,30 +1,18 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
-const database = new Database()
+import { routes } from './routes.js'
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/tasks') {
-    const tasks = database.select('tasks')
-    return res.end(JSON.stringify(tasks))
-  }
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if (method === 'POST' && url === '/tasks') {
-    const { title, description } = req.body
-    const task = {
-      id: 1,
-      title,
-      description,
-      completed_at: null,
-      created_at: new Date(),
-      updated_at: new Date(),
-    }
-    database.insert('tasks', task)
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
