fbff6d8be58a2d9b50bcaad5172817b49c587604
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-05-08 11:04:30 -0300

    use regex

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       const tasks = database.select('tasks')
       return res.end(JSON.stringify(tasks))
@@ -13,7 +14,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       const { title, description } = req.body
       const task = {
@@ -27,5 +28,12 @@ export const routes = [
       database.insert('tasks', task)
       return res.writeHead(201).end()
     }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      return res.end()
+    },
   },
 ]
